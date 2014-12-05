3480097f145d1bf9b569dd79219a7da45f5e6422
bordaigorl <emanuele.dosualdo@gmail.com>
2014-12-05 02:04:33 +0000

    Main: demo of NetView

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 define([
+  "underscore",
   "jquery",
   "d3",
-], function($, d3, events) {
+  "laborflows/model/network",
+  "laborflows/views/netview"
+], function(_, $, d3, Network, NetView) {
+
+  var svg = d3.select("svg#simulation-netview")
+      .attr("width", $(window).width())
+      .attr("height", $(window).height());
+
+  var network = new Network();
+
+  var firms = {};
+
+  _(["B", "C", "D", "E"]).each(function(d) {
+    firms[d] = {workers: [
+        {num: Math.random()*10, employed: true},
+        {num: Math.random()*10, employed: false}
+      ]};
+  });
+  firms["A"] = {workers: [{num:10}, {num:10, employed:false}]};
+
+  network.addFirm(firms);
+  network.link("A", ["B", "C", "D"], true);
+  network.link("E", ["D", "B"], true);
+
+  var view = new NetView(svg, network);
 
   console.log("LaborFlows rocks");
 
+  return {net: network, view: view};
+//
+// return network;
 });
