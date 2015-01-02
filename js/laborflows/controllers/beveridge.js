185696bc1910102057ad7b50aad6ccccbd5c89d1
bordaigorl <emanuele.dosualdo@gmail.com>
2015-01-02 02:31:27 +0100

    Beveridge Curve Chart module

---
+++ js/laborflows/controllers/beveridge.js
@@ -0,0 +1,77 @@
+define([
+  "underscore",
+  "jquery",
+  "chroma",
+  "laborflows/views/charts/scatterplot"
+], function(_, $, chroma, ScatterPlot){
+
+function Beveridge (domNode, network, options) {
+
+  if ( _(domNode).isString() ) domNode = d3.selectAll(domNode);
+  var $domNode = $(domNode[0]);
+
+  options = options || {};
+  _(options).defaults({samplingInterval: 10});
+
+  var samplingInterval = options.samplingInterval;
+
+  var chart = new ScatterPlot(domNode.select("svg"), {left: 30});
+
+  var time = 0,
+      series = -1,
+      color = chroma.brewer.Set1,
+      numOfColors = color.length;
+
+  var _bvAddSeries = function() {
+    series++;
+    chart.addSeries(series, {color: color[series % numOfColors]});
+  };
+  _bvAddSeries();
+
+  $domNode.find(".recycle").click(function() {
+    chart.removeSeries();
+    series = -1;
+    _bvAddSeries();
+  });
+
+  $domNode.find(".theme").click(function() {
+    _bvAddSeries();
+  });
+
+  var _bvUpdate = function(diff) {
+    if ( time === samplingInterval ) {
+      chart.addPoint(series, {
+        x: diff.unemployed,
+        y: _(diff.hiredWorkers).size()
+      });
+      time = 0;
+    } else time++;
+  };
+
+  $domNode.find(".sample").click(function() {
+    var btn = $(this);
+    if (btn.hasClass("play")) {
+      network.on("simulationStep", _bvUpdate);
+    } else {
+      network.off("simulationStep", _bvUpdate);
+    }
+    btn.toggleClass("play pause");
+  });
+
+  this.chart = function() {return chart;};
+
+  this.samplingInterval = function(i) {
+    if (!i) return samplingInterval;
+    samplingInterval = Math.abs(i);
+    time = Math.min(time, samplingInterval);
+    return this;
+  };
+
+  network.on("simulationStep", _bvUpdate);
+  network.on("networkChange", _bvAddSeries);
+
+}
+
+return Beveridge;
+
+});
